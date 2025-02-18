9009afd743f6361d690e10aa3b4df3342b6a2a2d
Nikitat0 <nikitatzero@yandex.ru>
2025-02-18 09:21:59 +0700

    Use member initializer lists in nstt2

--- nstt2/angem.cc
+++ nstt2/angem.cc
@@ -5,22 +5,12 @@
 using std::nullopt;
 using std::optional;
 
-Point::Point(double x, double y) {
-  this->x = x;
-  this->y = y;
-}
+Point::Point(double x, double y) : x(x), y(y) {}
 
-Line::Line(double a, double b, double c) {
-  this->a = a;
-  this->b = b;
-  this->c = c;
-}
+Line::Line(double a, double b, double c) : a(a), b(b), c(c) {}
 
-Line::Line(Point const& p, Point const& q) {
-  a = -(q.y - p.y);
-  b = q.x - p.x;
-  c = -(a * p.x + b * p.y);
-}
+Line::Line(Point const& p, Point const& q)
+    : a(-(q.y - p.y)), b(q.x - p.x), c(-(a * p.x + b * p.y)) {}
 
 Line Line::perpendicular(Point const& p) const {
   return Line(-b, a, b * p.x - a * p.y);
