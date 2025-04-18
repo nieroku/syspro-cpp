d868e1a9f645f9c15eb33ff936ec822341170f4c
nieroku <nikitatzero@yandex.ru>
2025-04-18 06:46:28 +0000

    Prettify

--- nstt11/prime.h
+++ nstt11/prime.h
@@ -6,6 +6,9 @@
 template <unsigned N>
 struct nth_prime;
 
+template <unsigned N>
+static const unsigned nth_prime_v = nth_prime<N>::value;
+
 template <>
 struct nth_prime<0>;
 
@@ -17,14 +20,17 @@ namespace internal {
 template <unsigned N, unsigned M>
 struct coprime : std::integral_constant<bool, (N % M) && (M % N)> {};
 
+template <unsigned N, unsigned M>
+static const bool coprime_v = coprime<N, M>::value;
+
 template <
     unsigned N,
     unsigned M = nth_prime<N - 1>::value + 1,
     unsigned I = N - 1>
 struct find_nth_prime : std::conditional_t<
-                            coprime<M, nth_prime<N - I>::value>::value,
+                            coprime_v<M, nth_prime_v<N - I>>,
                             find_nth_prime<N, M, I - 1>,
-                            find_nth_prime<N, M + 1>> {};
+                            find_nth_prime<N, M + 1>>::type {};
 
 template <unsigned N, unsigned M>
 struct find_nth_prime<N, M, 0> : std::integral_constant<unsigned, M> {};
@@ -33,8 +39,5 @@ struct find_nth_prime<N, M, 0> : std::integral_constant<unsigned, M> {};
 
 template <unsigned N>
 struct nth_prime : internal::find_nth_prime<N>::type {};
-
-template <unsigned N>
-static const unsigned nth_prime_v = nth_prime<N>::value;
 
 #endif
--- nstt11/prime_test.cc
+++ nstt11/prime_test.cc
@@ -3,10 +3,10 @@
 #include "gtest/gtest.h"
 
 TEST(NthPrimeTest, FindsNthPrime) {
-  EXPECT_EQ(nth_prime<1>(), 2);
-  EXPECT_EQ(nth_prime<2>(), 3);
-  EXPECT_EQ(nth_prime<3>(), 5);
-  EXPECT_EQ(nth_prime<126>(), 701);
-  EXPECT_EQ(nth_prime<256>(), 1619);
-  EXPECT_EQ(nth_prime<512>(), 3671);
+  EXPECT_EQ(nth_prime_v<1>, 2);
+  EXPECT_EQ(nth_prime_v<2>, 3);
+  EXPECT_EQ(nth_prime_v<3>, 5);
+  EXPECT_EQ(nth_prime_v<126>, 701);
+  EXPECT_EQ(nth_prime_v<256>, 1619);
+  EXPECT_EQ(nth_prime_v<512>, 3671);
 }
