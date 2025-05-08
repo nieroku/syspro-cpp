11f0aa1716089b26faec7fbf886dfd63a7e0c470
Nikitat0 <nikitatzero@yandex.ru>
2025-05-08 10:47:42 +0700

    Steal much better solution

--- nstt13/matcher.h
+++ nstt13/matcher.h
@@ -3,31 +3,11 @@
 
 #include <utility>
 
-namespace detail {
-
-template <typename Checker>
-struct Helper {
-  Checker& check;
-  bool found = false;
-  int idx = 0;
-
-  Helper(Checker& check) : check(check) {}
-
-  template <typename T>
-  Helper& operator&&(T&& arg) {
-    found = found || check(std::forward<T>(arg));
-    if (!found) idx++;
-    return *this;
-  }
-
-  operator int() { return idx; }
-};
-
-}  // namespace detail
-
 template <typename Checker, typename... Args>
 int getIndexOfFirstMatch(Checker&& check, Args&&... args) {
-  return (detail::Helper{check} && ... && std::forward<Args>(args));
+  int idx = -1;
+  (... || (++idx, check(std::forward<Args>(args))));
+  return idx;
 }
 
 #endif
